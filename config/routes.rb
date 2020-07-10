06ad20228f8b3c05f95d977ebe5c02510186943d
carlokryp <carlo.marmolejo@hotmail.com>
2020-07-10 12:19:15 -0500

    Creador del controlador de images

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root  'main#home'
   get 'prueba', to: 'control#saludo'
+  resources :images
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
