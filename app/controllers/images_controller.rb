bc060578ddb4e83cfa3c1ea17c95371205036659
carlokryp <carlo.marmolejo@hotmail.com>
2020-07-10 12:40:15 -0500

    Vista del modelo imagenes para agregar descripcion y modificacion en el controlador de 2 funciones para guardarlas en la base de datos

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,17 @@
-class ImagesController < 
+class ImagesController < ApplicationController
+    def new
+        
+    end
+
     def create
+        # render plain: params[:image].inspect
+        @image  = Image.new image_params
+        @image.save
+    end
+
+    private
+    def image_params
+        params.require(:image).permit(:descrition)
         
     end
 end
